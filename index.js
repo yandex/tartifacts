b1b0a8c231bb04c6287a7cc588dcb39522cfa911
blond <andrewblond@yandex.com>
2016-09-05 18:35:34 +0300

    refactor(defaults): init options in main file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const path = require('path');
+
 const promisify = require('es6-promisify');
 const each = promisify(require('async-each'));
 
@@ -8,19 +10,28 @@ const writeArtifact = require('./lib/write-artifact');
 /**
  * Creates and writes artifacts to fs.
  *
- * @param {object[]} artifacts          The artifacts info.
- * @param {object}   [options]          Options.
- * @param {string}   [options.root]     The path to root directory. By default is cwd.
- * @param {boolean}  [options.dot]      Include dotfiles.
+ * @param {object[]} artifacts                   The artifacts info.
+ * @param {object}   [options]                   Options.
+ * @param {string}   [options.root=cwd]          The path to root directory. By default is cwd.
+ * @param {boolean}  [options.dotFiles=false]    Include dotfiles.
+ * @param {boolean}  [options.emptyFiles=false]  Include empty files.
  * @returns {Promise}
  */
 module.exports = (artifacts, options) => {
     artifacts || (artifacts = []);
-    options || (options = {});
+
+    const defaults = {
+        root: process.cwd(),
+        dotFiles: false,
+        emptyFiles: false
+    };
+    const opts = Object.assign(defaults, options);
+
+    opts.root = path.resolve(opts.root);
 
     if (!Array.isArray(artifacts)) {
         artifacts = [artifacts];
     }
 
-    return each(artifacts, (artifact, callback) => writeArtifact(artifact, options, callback));
+    return each(artifacts, (artifact, callback) => writeArtifact(artifact, opts, callback));
 };
