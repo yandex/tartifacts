e170eece212c610bfe08180ab399e7d60eba442e
Andrew Abramov <andrewblond@yandex.com>
2017-11-21 16:01:14 +0300

    test(write-artifact): add case with name field

--- test/artifacts/write-artifact/dest.test.js
+++ test/artifacts/write-artifact/dest.test.js
@@ -9,6 +9,18 @@ const writeArtifact = require('../../../lib/artifacts').writeArtifact;
 
 test.afterEach(() => mockFs.restore());
 
+test('should create artifact dir by name', async t => {
+    mockFs({
+        'source-dir': {}
+    });
+
+    await writeArtifact({ name: 'artifact-dir', patterns: ['source-dir/'] });
+
+    const files = fs.readdirSync('artifact-dir');
+
+    t.deepEqual(files, ['source-dir']);
+});
+
 test('should create dest dir by dest', async t => {
     mockFs({
         'source-dir': {}
