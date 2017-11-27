638ebc86c0dbea0e0ba4d9916a21ea51f8a3f809
Andrew Abramov <andrewblond@yandex.com>
2017-11-27 10:12:37 +0300

    fix(write-artifact): reject promise if task error

--- test/artifacts/write-artifact/errors.test.js
+++ test/artifacts/write-artifact/errors.test.js
@@ -7,6 +7,13 @@ const writeArtifact = require('../../../lib/artifacts').writeArtifact;
 
 test.afterEach(() => mockFs.restore());
 
+test('should throw error if artifact task has error', t => {
+    t.throws(
+        writeArtifact({}),
+        /you should specify the dest path for artifact/
+    );
+});
+
 test('should throw error if include file does not exist', t => {
     mockFs({
         'source-dir': {}
